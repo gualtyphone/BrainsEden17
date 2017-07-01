f7ebdd2fc983883c9e7fe2e0423ed8bf2fb9ac6e
Nickstu14 <nickstu14@hotmail.co.uk>
2017-07-01 22:55:42 +0100

    Added Healthbar & uiplayer script to player prefab and mod ui manager

--- Brains Eden Project/Brains Eden 2017/Assets/UI/UiPlayerBar.cs
+++ Brains Eden Project/Brains Eden 2017/Assets/UI/UiPlayerBar.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class UiPlayerBar : MonoBehaviour
 {
-    public GameObject m_player;
-    // Use this for initialization
+    public EnergyContainer m_Ec;
+
+    public UiBar[] m_UiBar;
+
+    public Image[] m_Bar;
+
     void Start()
     {
-       // m_player = gameObject.Ge
+        m_Ec = gameObject.GetComponent<EnergyContainer>();
+        m_UiBar = gameObject.GetComponentsInChildren<UiBar>();
+
+      /*  m_Bar = gameObject.GetComponentsInChildren<Image>();
+        for (int i = 0; i < m_Bar.Length; i++)
+        {
+            m_Bar[i].color = gameObject.GetComponent<PlayerController>().playerColor;
+        }*/
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-
+        for (int i = 0; i < m_UiBar.Length; i++)
+        {
+            m_UiBar[i].SetHealthBar(m_Ec.energy, m_Ec.maxEnergy);
+        }
     }
 }
