389d0fa709e75ec13ee50bda0fedd1104effa87a
Gualt_ <gualtyphone@me.com>
2017-07-01 23:49:58 +0100

    LightningTry

---
+++ Brains Eden Project/Brains Eden 2017/Assets/BarLightningController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarLightningController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject LightningEnd;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<LightningSpawner>().endPoint = LightningEnd.transform.position;
+        GetComponent<LightningSpawner>().midPoint = (transform.position + LightningEnd.transform.position)/2;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
